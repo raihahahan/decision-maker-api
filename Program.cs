6ee329da81c9b25be59bfd5c858d2da07ca33967
Muhd Raihan <muhdraihan1140@gmail.com>
2022-10-20 16:17:56 +0800

    updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using DecisionMakerApi.Features.RandomDecision.Models;
+using DecisionMakerApi.Features.ConditionalDecision.Models;
+using DecisionMakerApi.Features.WeightedDecision.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registed database context
+builder.Services.AddDbContext<RandomDecisionContext>(opt =>
+    opt.UseInMemoryDatabase("RandomDecisionList"));
+builder.Services.AddDbContext<ConditionalDecisionContext>(opt =>
+    opt.UseInMemoryDatabase("ConditionalDecisionList"));
+builder.Services.AddDbContext<WeightedDecisionContext>(opt =>
+    opt.UseInMemoryDatabase("WeightedDecisionList"));
 
 var app = builder.Build();
 
