de0f70d19a69bbff93511e280be67d1760e36074
Muhd Raihan <muhdraihan1140@gmail.com>
2022-10-21 23:00:52 +0800

    updated Choice.cs

--- Source/Common/Domains/Choice.cs
+++ Source/Common/Domains/Choice.cs
@@ -2,6 +2,21 @@ namespace DecisionMakerApi.Common.Domains;
 
 public class Choice
 {
-    public long Id { get; set; }
-    public string Name { get; set; } = null!;
+    private long id;
+    private string _Name;
+    public Choice(long Id, string Name) 
+    {
+        id = Id;
+        _Name = Name;
+    }
+    public long Id 
+    { 
+        get { return id; }
+        set { id = value; }
+    }
+    public string Name 
+    {
+        get { return _Name; }
+        set{ _Name = value; }
+    }
 }
