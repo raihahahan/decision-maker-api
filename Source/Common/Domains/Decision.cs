2709dd4c7a253846b08626555605ad34db8d8e67
Muhd Raihan <muhdraihan1140@gmail.com>
2022-10-24 10:07:36 +0800

    added createdAt property

--- Source/Common/Domains/Decision.cs
+++ Source/Common/Domains/Decision.cs
@@ -3,9 +3,15 @@ namespace DecisionMakerApi.Common.Domains;
 public abstract class Decision
 {
     private List<Choice> _choices = new List<Choice>();
+    private DateTime createdAt = new DateTime();
 
     public long Id { get; set; }
     public string Name { get; set; } = null!;
+    public DateTime CreatedAt 
+    { 
+        get { return createdAt; }
+        set { createdAt = value; }
+    }
     public List<Choice> Choices
     {
         get { return _choices; }
