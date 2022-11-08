bed09bae53bd08731080aa835059a33291750518
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-08 10:58:56 +0800

    changed to shorthand syntax

--- Source/Common/Domains/Choice.cs
+++ Source/Common/Domains/Choice.cs
@@ -2,21 +2,6 @@ namespace DecisionMakerApi.Common.Domains;
 
 public class Choice
 {
-    private long id;
-    private string _Name;
-    public Choice(long Id, string Name) 
-    {
-        id = Id;
-        _Name = Name;
-    }
-    public long Id 
-    { 
-        get { return id; }
-        set { id = value; }
-    }
-    public string Name 
-    {
-        get { return _Name; }
-        set{ _Name = value; }
-    }
+    public long Id { get; set; }
+    public string Name { get; set; } = null!;
 }
