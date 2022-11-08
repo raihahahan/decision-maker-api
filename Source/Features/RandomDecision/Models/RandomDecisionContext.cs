bfcc9bcc7253f60dfca21ef42ab5a65ce4a22d9b
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-08 11:00:51 +0800

    added one to many relationship for randomdecision

--- Source/Features/RandomDecision/Models/RandomDecisionContext.cs
+++ Source/Features/RandomDecision/Models/RandomDecisionContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using DecisionMakerApi.Common.Domains;
 using System.Diagnostics.CodeAnalysis; 
 
 namespace DecisionMakerApi.Features.RandomDecision.Models;
@@ -7,4 +8,13 @@ public class RandomDecisionContext : DbContext
 { 
     public RandomDecisionContext(DbContextOptions<RandomDecisionContext> options) : base(options) { } 
     public DbSet<RandomDecisionItem> RandomDecisionItems { get; set; } = null!; 
+    public DbSet<Choice> Choices { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RandomDecisionItem>()
+            .HasMany(d => d.Choices)
+            .WithOne();
+    }
+
 } 
