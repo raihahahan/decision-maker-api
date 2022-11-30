71abf73734f5cd3fd11d26e6b211cd68b31621b4
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-30 11:56:24 +0800

    added refId property to Choice.cs

--- Source/Common/Domains/Choice.cs
+++ Source/Common/Domains/Choice.cs
@@ -5,4 +5,5 @@ public class Choice
     public long Id { get; set; }
     public string Name { get; set; } = null!;
     public long DecisionId { get; set; }
+    public string? RefId { get; set; }
 }
