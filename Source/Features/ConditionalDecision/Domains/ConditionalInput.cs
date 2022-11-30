af122a30d4236f412294e539fb1ef23d5f32e3ba
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-30 11:57:48 +0800

    added foreignId to conditionalInput

--- Source/Features/ConditionalDecision/Domains/ConditionalInput.cs
+++ Source/Features/ConditionalDecision/Domains/ConditionalInput.cs
@@ -3,4 +3,5 @@ namespace DecisionMakerApi.Features.ConditionalDecision.Domains;
 public class ConditionalInput : Condition
 {
     public Boolean Value { get; set; }
+    public long ForeignId { get; set; }
 }
