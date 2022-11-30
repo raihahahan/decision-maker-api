26c0e836c71eccbdc34898570c86118135c629e5
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-30 11:58:12 +0800

    modified conditionalDecisionContext to reflect many-one relationships

--- Source/Features/ConditionalDecision/Models/ConditionalDecisionContext.cs
+++ Source/Features/ConditionalDecision/Models/ConditionalDecisionContext.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using DecisionMakerApi.Common.Domains;
 using System.Diagnostics.CodeAnalysis; 
+using DecisionMakerApi.Features.ConditionalDecision.Domains;
+using DecisionMakerApi.Features.ConditionalDecision.Models;
 
 namespace DecisionMakerApi.Features.ConditionalDecision.Models;
 
@@ -7,4 +10,53 @@ public class ConditionalDecisionContext : DbContext
 { 
     public ConditionalDecisionContext(DbContextOptions<ConditionalDecisionContext> options) : base(options) { } 
     public DbSet<ConditionalDecisionItem> ConditionalDecisionItems { get; set; } = null!; 
+
+    public DbSet<Condition> Conditions { get; set; } = null!;
+    public DbSet<InnerItem> Include { get; set; } = null!;
+    public DbSet<InnerItem> Exclude { get; set; } = null!;
+
+    public DbSet<Choice> Choices { get; set; } = null!;
+
+    public DbSet<ConditionalInputItem> ConditionalInputItems { get; set; } = null!;
+    public DbSet<ConditionalInput> ConditionalInputs { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) 
+    {
+        modelBuilder.Entity<ConditionalDecisionItem>()
+            .HasMany(d => d.Choices)
+            .WithOne()
+            .HasForeignKey(i => i.DecisionId);
+
+        modelBuilder.Entity<ConditionalDecisionItem>()
+            .HasMany(d => d.Conditions)
+            .WithOne()
+            .HasForeignKey(i => i.DecisionId);
+
+        modelBuilder.Entity<Condition>()
+            .HasMany(d => d.Include)
+            .WithOne()
+            .HasForeignKey(i => i.ConditionId);
+        
+        modelBuilder.Entity<Condition>()
+            .HasMany(d => d.Exclude)
+            .WithOne()
+            .HasForeignKey(i => i.ConditionId);
+
+        // Input
+
+        modelBuilder.Entity<ConditionalInputItem>()
+            .HasMany(d => d.ConditionalInputs)
+            .WithOne()
+            .HasForeignKey(i => i.ForeignId);
+
+        modelBuilder.Entity<ConditionalInput>()
+            .HasMany(d => d.Include)
+            .WithOne()
+            .HasForeignKey(i => i.ConditionId);
+        
+        modelBuilder.Entity<ConditionalInput>()
+            .HasMany(d => d.Exclude)
+            .WithOne()
+            .HasForeignKey(i => i.ConditionId);
+    }
 } 
