a23cebfb9a73f62d33732834d65c3bcef1bcf30d
Muhd Raihan <muhdraihan1140@gmail.com>
2022-12-03 23:17:00 +0800

    added logging

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using DecisionMakerApi.Features.WeightedDecision.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services to the container.
 builder.Services.AddCors(options =>
